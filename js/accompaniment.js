ed6cea117f571f89ff0c3885e15f5f58e008890d
weydson <weydson.macedo@gmail.com>
2019-09-11 01:57:02 -0300

    testando hh

--- js/accompaniment.js
+++ js/accompaniment.js
@@ -1,7 +1,8 @@
 var kick = new Tone.Player("https://cdn.jsdelivr.net/gh/Tonejs/Tone.js/examples/audio/505/kick.mp3").toMaster();
 var snare = new Tone.Player("https://cdn.jsdelivr.net/gh/Tonejs/Tone.js/examples/audio/505/snare.mp3").toMaster();
-var hh = new Tone.Player("notes/Closed-Hi-Hat-1.wav").toMaster();
-
+var hh = new Tone.Player("notes/Closed-Hi-Hat-1.wav").toMaster(); 
+var openHH = new Tone.Player("notes/hihat_004b.wav").toMaster();
+stop = true;
 
   function playDrum(){
     // var p1 = new Tone.Players({
@@ -15,18 +16,20 @@ var hh = new Tone.Player("notes/Closed-Hi-Hat-1.wav").toMaster();
      //hh.connect(soundFix.volume);
     //  snare.connect(soundFix.volume);
      Tone.context.latencyHint = 'fastest';  
-     Tone.Transport.bpm.value = 120;
+     Tone.Transport.bpm.value = 100;
     //  Tone.Sequence.defaults = "8n";
      var seq = new Tone.Sequence(function(time,idx){
         hh.start();    
-        if([0,4,5,8,12,13].indexOf(idx) >=0)
+        if([0,4,7,8,12].indexOf(idx) >=0)
            kick.start();
         if([2,6,10,14].indexOf(idx) >=0)
            snare.start();
+         if([5,13].indexOf(idx) >=0) 
+            openHH.start();   
       //  console.log(idx);
       //  console.log(time);
       event.humanize = true;
-     },[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],"8n");
+     },[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],"6n");
      Tone.Transport.start('+0.2');
      seq.start();
   }
