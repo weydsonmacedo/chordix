3c5e8dd6e4a33009fba466d9a46ba53567160900
Weydson Ferreira Macedo da Silva <weydson.silva@prodeb.local>
2019-09-17 18:49:27 -0300

    basico bateria pronto

--- js/accompaniment.js
+++ js/accompaniment.js
@@ -3,6 +3,22 @@ var snare = new Tone.Player("https://cdn.jsdelivr.net/gh/Tonejs/Tone.js/examples
 var hh = new Tone.Player("notes/Closed-Hi-Hat-1.wav").toMaster(); 
 var openHH = new Tone.Player("notes/hihat_004b.wav").toMaster();
 stop = true;
+var seq ;
+
+function structDrum(notes){
+  Tone.context.latencyHint = 'fastest'; 
+  Tone.Transport.start('+0.2');
+  seq = new Tone.Sequence(function(time,idx){
+        hh.start();    
+        if([0,4,7,8,12].indexOf(idx) >=0)
+           kick.start();
+        if([2,6,10,14].indexOf(idx) >=0)
+           snare.start();
+         if([5,13].indexOf(idx) >=0) 
+            openHH.start();   
+      event.humanize = true;
+     },[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],notes);
+}
 
   function playDrum(){
     // var p1 = new Tone.Players({
@@ -15,7 +31,6 @@ stop = true;
     //  kick.connect(soundFix.volume);
      //hh.connect(soundFix.volume);
     //  snare.connect(soundFix.volume);
-
      var notes = $('#select_drum option:selected').val();
      var bpm;
      if($('#bpm').val()){
@@ -23,35 +38,25 @@ stop = true;
      } else{
        bpm = 100;
      }
-
-     Tone.context.latencyHint = 'fastest';  
      Tone.Transport.bpm.value = bpm;
+
+      
+   
     
-     var seq = new Tone.Sequence(function(time,idx){
-        hh.start();    
-        if([0,4,7,8,12].indexOf(idx) >=0)
-           kick.start();
-        if([2,6,10,14].indexOf(idx) >=0)
-           snare.start();
-         if([5,13].indexOf(idx) >=0) 
-            openHH.start();   
-      event.humanize = true;
-     },[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],notes);
-     Tone.Transport.start('+0.2');
-     seq.start();
+     toControl(notes);
+
+    
   }
   
-function toControl(drums) {
-  if (stop) {
-    stop = false;
-    drums.play('blast');
-    drums.rate(1);
-    disable(true);
-  } else {
-    stop = true;
-    drums.stop();
-    disable(false);
-  }
+function toControl(notes) {
+  if (!stop) {
+      stop = true;
+      seq.stop();
+    }else{
+      stop = false;
+      structDrum(notes);
+      seq.start();
+    }
 }
 
 
