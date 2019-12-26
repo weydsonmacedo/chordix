8bcf4b7c22e1434f6203064334680f6fb1de59f3
weydson <weydson.macedo@gmail.com>
2019-12-26 20:45:59 -0200

    testando drum and bass

--- js/Bass.js
+++ js/Bass.js
@@ -11,28 +11,38 @@ class Bass {
         this.soundVolume = [this.e2,this.a2,this.d3,this.g3,this.c4];
         this.volume();
         this.note;
+        this.seq;
     }
     _toStructBass = function (bass,note) {
 		Tone.context.latencyHint = 'fastest';
 		Tone.Transport.start('+0.2');
-	//	 return new Tone.Sequence(function (time, idx) {
-	//	 	bass.acusticNotes[bass.note[1]].triggerAttack(Math.floor(bass.note[0]), "+0", 1);
-      //   }, [0], "8n");
+		 return new Tone.Sequence(function (time, idx) {
+             if([0,3,8,11,16,19,24,27].indexOf(idx) >= 0) {
+                
+                 bass.acusticNotes[bass.note[1]].triggerAttackRelease(Math.floor(bass.note[0]+4),"0.2", "+0", 1);
+             }
+             if([4,6,12,14,20,22,28,30].indexOf(idx) >= 0) {
+                
+                bass.acusticNotes[bass.note[1]].triggerAttackRelease(Math.floor(bass.note[0]),"0.1", "+0", 1);
+            }
+             // 0 : tonica / 4: terça maior / 3: terça menor / 7: quinta maior/menor  / diminuta 6/ 
+        }, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31], "16n");
 
-        this.acusticNotes[note[1]].triggerAttackRelease(Math.floor(note[0]),"0.5", "+0", 1);
+     //   this.acusticNotes[note[1]].triggerAttackRelease(Math.floor(note[0]),"0.5", "+0", 1);
 
 	};
 
     play = function(shape,bass){
         this.note = shape.map((x,index) => { return [x,index] }).filter(x =>{ return x[0] != 'X'})[0];
-        var teste = this._toStructBass(bass,this.note);
-         //   teste.start();
+        this.seq = this._toStructBass(bass,this.note);
+        this.seq.start();
     }
     stop = function(){
-
+        if(this.seq)
+            this.seq.stop();
     }
    clear  = function(bass) {
-    bass.stop
+        bass.stop();
         this.e2.triggerRelease();
         this.a2.triggerRelease();
         this.d3.triggerRelease();
